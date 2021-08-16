14704eb77ee2123037beb717e4310fc3c7e4701f
JacobBeckfeld <jacbeck456@gmail.com>
2021-08-16 15:38:38 -0500

    added ignore file

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -32,6 +32,7 @@ const typeDefs = gql`
     type Mutation {
         createUser(username: String!, email: String!, password: String!): User
         login(email: String, password: String!): Auth
+        removeBook(_id: Int): User
     }
 `
 
